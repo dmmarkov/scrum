829dfc3e41f12f753dc51d3e48b5bea9b8d78f06
arch1tect0r <dmitri__13@mail.ru>
2018-11-08 21:29:05 +0300

    Fix misprints

--- project/level1/Level1.gd
+++ project/level1/Level1.gd
@@ -4,13 +4,13 @@ signal player_died
 signal player_win
 
 func _ready():
-	$Mark.connect("player_died",self,"_on_die")
-	$Mark.connect("player_win",self,"_on_win")
+	$Mark.connect("player_died", self, "_on_die")
+	$Mark.connect("player_win", self, "_on_win")
 
-func _on_spawn_soliders_timer_timeout():
-	var solider = preload("res://common_assets/Enemies/solider1.tscn").instance()
-	solider.position = $spawn_soliders.global_position 
-	add_child(solider) 
+func _on_spawn_soldiers_timer_timeout():
+	var soldier = preload("res://common_assets/Enemies/soldier1.tscn").instance()
+	soldier.position = $spawn_soldiers.global_position 
+	add_child(soldier) 
 	
 func _on_die():
 	emit_signal("player_died", self)
