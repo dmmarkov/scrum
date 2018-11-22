f9bcbab76f30566d78a580981b827b11f7065fed
arch1tect0r <dmitri__13@mail.ru>
2018-11-22 23:22:13 +0300

    Format check script improved

--- tools/check-files-format.sh
+++ tools/check-files-format.sh
@@ -1,16 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Files with trailing spaces"
 
-grep --exclude-dir=.git --include=*.gd  --include=*.tscn --recursive '[[:blank:]]$' project || exit 0
+grep --exclude-dir=.git --include=*.gd  --include=*.tscn --recursive '[[:blank:]]$' project
 
-echo "Fix this list of files!"
+if test $? -eq 0; then
+    echo "Fix this list of files!"
+    exit 1
+else
+    echo "No files with trailing spaces"
+fi
 
 echo ""
 
 echo "Files without newline at the end"
 
-find project -type f -name *.gd -exec bash -c 'test -z "$(tail -c 1 {})" || echo {}' \;
+FILES=$(find project -type f -name *.gd -exec bash -c 'test -z "$(tail -c 1 {})" || echo {}' \;)
+
+if test -z $FILES; then
+    echo "No files without newline at the end"
+    exit 0
+else
+    echo $FILES
+fi
 
 echo "Fix this list of files!"
 
