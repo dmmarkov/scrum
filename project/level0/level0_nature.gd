0485730bd23acf9ea6285ec25dfd25914b6d22d0
arch1tect0r <dmitri__13@mail.ru>
2018-11-27 00:30:09 +0300

    Working paralax backround but with some limitations

--- project/level0/level0_nature.gd
+++ project/level0/level0_nature.gd
@@ -1,15 +1,28 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+signal player_died
+signal player_win
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	$player.connect("player_died", self, "_on_die")
+	$player.connect("player_win", self, "_on_win")
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _on_spawn_soldiers_timer_timeout():
+	var soldier = preload("res://common_assets/Enemies/soldier1.tscn").instance()
+	soldier.position = $spawn_soldiers.global_position
+	add_child(soldier)
+
+func _on_die():
+	emit_signal("player_died", self)
+
+func _on_win():
+	emit_signal("player_win", self)
+
+func _on_viewport_resized():
+	var viewport = get_node("/level1")
+	var height = viewport.get_rect().size.y
+	var width = viewport.get_rect().size.x
+
+	# Set viewpoint to run at 1080px tall, but adjust width to match aspect ratio of screen
+	viewport.set_size_override(true, Vector2(1080*width/height,1080))
+	viewport.set_size_override_stretch(true)
